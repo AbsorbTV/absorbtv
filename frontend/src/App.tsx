40176b4bec3c64c06a67f14f11edd9045a0bf6a6
Roger Andersen <hexjelly@hexjelly.com>
2019-08-04 05:49:32 +0200

    fix: remove theme import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import theme from './config/theme';
 import NavBar from './components/NavBar';
 
 const App = () => (
